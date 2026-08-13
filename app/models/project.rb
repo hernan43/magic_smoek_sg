74640632cc076bee4c3f85b6345534c047df06cc
Ray Hernandez <hernan43@gmail.com>
2026-08-13 14:41:31 -0400

    Add basic pre-sanitization on rich text

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,19 @@
 class Project < ApplicationRecord
+    before_save :sanitize_description
+
     belongs_to :user
     
     validates :name, presence: true
     validates :description, presence: true
+
+    private
+
+    def sanitize_description
+        self.description = ActionController::Base.helpers.sanitize(
+        description,
+        tags: %w[a blockquote br em h1 h2 h3 img li ol p s strong u ul],
+        attributes: %w[href src]
+        )
+    end
+
 end
