8d525348287bf72ef92f8f5b6a61496bca67b791
Ray Hernandez <hernan43@gmail.com>
2026-08-13 22:39:33 -0400

    Delete vestigal code

--- app/views/users/dashboard.jsx
+++ app/views/users/dashboard.jsx
@@ -11,15 +11,6 @@ export default function UsersDashboard() {
 
   return (
     <>
-      {current_user.projects.length === 0 ? (
-        <p>You have no projects yet.</p>
-      ) : (
-        <div className="project-list">
-        {current_user.projects.map(project => (
-            <ProjectCard key={project.id} project={project} />
-        ))}
-      </div>
-      )}
     </>
   )
 }
